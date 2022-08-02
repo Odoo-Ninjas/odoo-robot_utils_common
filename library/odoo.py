7fc172809ed583d32c7c170ca3442a9ae41dc8a4
cicd <cicd>
2022-08-02 10:07:00 +0200

    [main]

--- library/odoo.py
+++ library/odoo.py
@@ -48,7 +48,10 @@ def convert_args(method):
 
 class odoo(object):
 
-    def technical_testname(self, testname):
+    def technical_testname(self):
+        test_name = BuiltIn().get_variable_value("${TEST NAME}")
+        if not test_name:
+            test_name = BuiltIn().get_variable_value("${SUITE NAME}")
         testname = testname.lower().replace(" ", "_")
         return testname
 
@@ -183,10 +186,7 @@ class odoo(object):
         suffix = filepath.suffix
 
         # replace some environment variables:
-        test_name = BuiltIn().get_variable_value("${TEST NAME}")
-        if not test_name:
-            test_name = BuiltIn().get_variable_value("${SUITE NAME}")
-        test_name = self.technical_testname(test_name)
+        test_name = self.technical_testname()
         content = content.replace("${CURRENT_TEST}", test_name)
 
         module_name = module_name.lower()
